a7f18f5d243221a7283b8bf0e60e3a070920f81b
SureshDonthula <sureshdonthula99@gmail.com>
2022-09-23 22:19:08 +0530

    first commit

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Integer[] arr =  {1,2,12,123,1234};
+		List<Integer> l = Arrays.asList(arr);
+		l.stream().map(n->n+"")
+					.filter(n->n.startsWith("1"))
+					.forEach(System.out::println);
+				
+
+	}
+
+}
